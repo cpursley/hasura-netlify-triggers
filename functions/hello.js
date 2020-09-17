c1a3d730d7c66f0f02ed737c82f1a8194216dc84
Chase Pursley <chasepursley@gmail.com>
2020-09-16 20:16:25 -0400

    clean up.

--- functions/hello.js
+++ functions/hello.js
@@ -1,30 +1,4 @@
-/* Handle Hasura event trigger
- * This is what the structure of the event payload looks like
- *
- * {
- *   "event": {
- *       "op": "<op-name>",
- *       "data": {
- *         "old": <old-object>,
- *         "new": <new-object>
- *       }
- *   },
- *   "created_at": "<timestamp>",
- *   "id": "<uuid>",
- *   "trigger": {
- *       "name": "<name-of-trigger>",
- *       "id": "<event-uuid>"
- *   },
- *   "table":  {
- *       "schema": "<schema-name>",
- *       "name": "<table-name>"
- *   }
- * }
- *
- */
-
 exports.handler = (lambdaEvent, context, callback) => {
-  // Get the email of the user
   const hasuraTriggerPayload = JSON.parse(lambdaEvent.body);
 
   console.log(hasuraTriggerPayload);
